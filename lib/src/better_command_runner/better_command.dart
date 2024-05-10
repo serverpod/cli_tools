5ba20d00f9bd4786ccab32f2397fec2c28ac2c0e
Alexander Sandor <alexander@serverpod.dev>
2024-05-10 10:45:34 +0200

    feat: introduce better command to help print usage and set wrap text column for arg parser.

---
+++ lib/src/better_command_runner/better_command.dart
@@ -0,0 +1,22 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:cli_tools/better_command_runner.dart';
+
+abstract class BetterCommand extends Command {
+  final PassMessage? _logInfo;
+  final ArgParser _argParser;
+
+  BetterCommand({
+    PassMessage? logInfo,
+    int? wrapTextColumn,
+  })  : _logInfo = logInfo,
+        _argParser = ArgParser(usageLineLength: wrapTextColumn);
+
+  @override
+  ArgParser get argParser => _argParser;
+
+  @override
+  void printUsage() {
+    _logInfo?.call(usage);
+  }
+}
---
+++ test/better_command_test.dart
@@ -0,0 +1,42 @@
+import 'package:cli_tools/better_command_runner.dart';
+import 'package:test/test.dart';
+
+class MockCommand extends BetterCommand {
+  static String commandName = 'mock-command';
+
+  MockCommand({super.logInfo}) {
+    argParser.addOption(
+      'name',
+      defaultsTo: 'serverpod',
+      allowed: <String>['serverpod', 'stockholm'],
+    );
+  }
+
+  @override
+  String get description => 'Mock command used for testing';
+
+  @override
+  String get name => commandName;
+
+  @override
+  void run() {}
+}
+
+void main() {
+  group('Given a better command registered in the better command runner', () {
+    var infos = <String>[];
+    var betterCommand =
+        MockCommand(logInfo: (String message) => infos.add(message));
+    var runner = BetterCommandRunner('test', 'test project')
+      ..addCommand(betterCommand);
+
+    test(
+        'when running command option --help flag then usage is printed to commands logInfo',
+        () async {
+      await runner.run([MockCommand.commandName, '--help']);
+
+      expect(infos, hasLength(1));
+      expect(infos.first, betterCommand.usage);
+    });
+  });
+}
