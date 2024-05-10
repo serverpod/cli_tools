746b112f028ed5ec4c284c4d7917373a9573bafb
Alexander Sandor <alexander@serverpod.dev>
2024-05-10 14:06:02 +0200

    fix!: Remove callback and throw specific exceptions for pub api client errors.

--- lib/package_version.dart
+++ lib/package_version.dart
@@ -1,4 +1,5 @@
 library package_version;
 
 export 'src/package_version/package_version.dart';
+export 'src/package_version/pub_api_client_exceptions.dart';
 export 'src/package_version/pub_api_client.dart';
