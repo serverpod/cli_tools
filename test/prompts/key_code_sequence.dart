84ce6ce607791b830777e38542abc61471def07a
Hampus Lavin <hampus.lavin@gmail.com>
2025-01-16 15:31:55 +0100

    test: add tests and helper for new features

---
+++ test/prompts/key_code_sequence.dart
@@ -0,0 +1,4 @@
+import 'package:cli_tools/src/prompts/key_codes.dart';
+
+var arrowUpSequence = [...KeyCodes.leadingArrowEscapes, KeyCodes.arrowUp];
+var arrowDownSequence = [...KeyCodes.leadingArrowEscapes, KeyCodes.arrowDown];
