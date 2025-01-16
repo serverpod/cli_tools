24835e3db21169f61feb67e8b412f59d0efa26ef
Hampus Lavin <hampus.lavin@gmail.com>
2025-01-16 16:40:44 +0100

    feat: add input prompt component

---
+++ test/prompts/input_test.dart
@@ -0,0 +1,150 @@
+import 'package:cli_tools/cli_tools.dart';
+import 'package:cli_tools/src/prompts/input.dart';
+import 'package:test/test.dart';
+
+import '../test_utils/io_helper.dart';
+
+void main() {
+  var logger = StdOutLogger(LogLevel.debug);
+
+  test(
+      'Given input prompt '
+      'when providing valid input "hello" '
+      'then should return "hello"', () async {
+    late Future<String> result;
+    await collectOutput(
+      stdinLines: ['hello'],
+      () {
+        result = input(
+          'Enter something',
+          logger: logger,
+        );
+      },
+    );
+
+    await expectLater(
+      result,
+      completion('hello'),
+    );
+  });
+
+  test(
+      'Given input prompt '
+      'when providing empty input with default value "default" '
+      'then should return "default"', () async {
+    late Future<String> result;
+    await collectOutput(
+      stdinLines: [''],
+      () async {
+        result = input(
+          'Enter something',
+          defaultValue: 'default',
+          logger: logger,
+        );
+      },
+    );
+
+    await expectLater(
+      result,
+      completion('default'),
+    );
+  });
+
+  test(
+      'Given input prompt '
+      'when providing empty input without default value '
+      'then should return an empty string', () async {
+    late Future<String> result;
+    await collectOutput(
+      stdinLines: [''],
+      () async {
+        result = input(
+          'Enter something',
+          logger: logger,
+        );
+      },
+    );
+
+    await expectLater(
+      result,
+      completion(''),
+    );
+  });
+
+  test(
+      'Given input prompt '
+      'when providing valid input "TEST" '
+      'then should return "TEST"', () async {
+    late Future<String> result;
+    await collectOutput(
+      stdinLines: ['TEST'],
+      () {
+        result = input(
+          'Enter something',
+          logger: logger,
+        );
+      },
+    );
+
+    await expectLater(
+      result,
+      completion('TEST'),
+    );
+  });
+
+  test(
+      'Given input prompt '
+      'when providing input with leading and trailing spaces '
+      'then should trim string', () async {
+    late Future<String> result;
+    await collectOutput(
+      stdinLines: ['   hello   '],
+      () async {
+        result = input(
+          'Enter something',
+          logger: logger,
+        );
+      },
+    );
+
+    await expectLater(
+      result,
+      completion('hello'),
+    );
+  });
+
+  test(
+      'Given input prompt '
+      'when providing defult value '
+      'then should display prompt with default value', () async {
+    var (:stdout, :stderr, :stdin) = await collectOutput(
+      stdinLines: [''],
+      () async {
+        await input(
+          'Enter something',
+          defaultValue: 'default',
+          logger: logger,
+        );
+      },
+    );
+
+    expect(stdout.output, 'Enter something (default): ');
+  });
+
+  test(
+      'Given input prompt '
+      'when providing no default value '
+      'then should not display a default description', () async {
+    var (:stdout, :stderr, :stdin) = await collectOutput(
+      stdinLines: ['value'],
+      () async {
+        await input(
+          'Enter something',
+          logger: logger,
+        );
+      },
+    );
+
+    expect(stdout.output, 'Enter something: ');
+  });
+}
--- test/prompts/key_code_sequence.dart
+++
@@ -1,4 +0,0 @@
-import 'package:cli_tools/src/prompts/key_codes.dart';
-
-var arrowUpSequence = [...KeyCodes.leadingArrowEscapes, KeyCodes.arrowUp];
-var arrowDownSequence = [...KeyCodes.leadingArrowEscapes, KeyCodes.arrowDown];
