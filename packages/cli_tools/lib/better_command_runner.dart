dd5fcbc6d29760df1f5025dc48501dd8dbd7164d
Christer <christer@serverpod.dev>
2025-09-27 21:55:33 +0200

    chore(cli_tools): Export public completion target data types

--- packages/cli_tools/lib/better_command_runner.dart
+++ packages/cli_tools/lib/better_command_runner.dart
@@ -1,3 +1,4 @@
 export 'src/better_command_runner/better_command.dart';
 export 'src/better_command_runner/better_command_runner.dart';
+export 'src/better_command_runner/completion/completion.dart';
 export 'src/better_command_runner/exit_exception.dart';
