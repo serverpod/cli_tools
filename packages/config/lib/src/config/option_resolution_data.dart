a4999a2c57e78af6dbaba96f99dc6e7aca152872
Indraneel Rajeevan <105813454+indraneel12@users.noreply.github.com>
2025-10-14 00:50:26 +0530

    docs(`OptionResolutionData`): Missing docs added Comprehensively covers the purpose of this new Mixin.

--- packages/config/lib/src/config/option_resolution_data.dart
+++ packages/config/lib/src/config/option_resolution_data.dart
@@ -1,10 +1,18 @@
 import 'source_type.dart';
 
+/// Provides significant metadata about an Option once it is resolved.
 base mixin OptionResolutionData<V> {
-  String? get stringValue;
+  /// The resolved value of this Option.
   V? get value;
-  String? get error;
+
+  /// The source from where this Option has been resolved.
   ValueSourceType get source;
+
+  /// A string representation of this Option's value.
+  String? get stringValue;
+
+  /// An Error that may have been encountered during Option Resolution.
+  String? get error;
 
   /// Whether there was an error during resolving this Option.
   bool get hasError => error != null;
