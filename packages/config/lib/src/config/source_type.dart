aa3718e791862dcdb82c7c57aab30160f0fd83ee
Christer <christer@serverpod.dev>
2025-11-09 13:56:29 +0100

    docs(config): Added ValueSourceType doc comments

--- packages/config/lib/src/config/source_type.dart
+++ packages/config/lib/src/config/source_type.dart
@@ -1,10 +1,23 @@
 /// The type of source that an option's value was resolved from.
 enum ValueSourceType {
+  /// The option has no value.
   noValue,
+
+  /// The value was preset via code rather than normally resolved.
   preset,
+
+  /// The value was parsed from command-line arguments.
   arg,
+
+  /// The value was parsed from environment variables.
   envVar,
+
+  /// The value was parsed from a configuration file.
   config,
+
+  /// The value was provided by a custom callback.
   custom,
+
+  /// The value is the default for the option.
   defaultValue,
 }
